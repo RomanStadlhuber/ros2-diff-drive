477694c52f243483b13d6178a346ce80577d79d8
RomanStadlhuber <roman.stadlhuber@gmail.com>
2020-11-13 15:49:28 +0100

    create starting point for wheel dirver package

---
+++ robot_ws/src/wheel_control/src/wheel_driver.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+#include <string>
+#include <functional>
+
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world wheel_control package\n");
+  return 0;
+}
