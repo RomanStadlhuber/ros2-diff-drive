3ce12deeafc2be46ce9b1d13dfcb9c86aa670844
RomanStadlhuber <roman.stadlhuber@gmail.com>
2020-11-22 01:02:09 +0100

    apply parameter file in launch files, refactor launch file node names to contain mapped namespaces

--- robot_ws/launch/minimal.launch.py
+++ robot_ws/launch/minimal.launch.py
@@ -1,20 +1,30 @@
 import os
-
+import pathlib
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
+    param_file_path = str(pathlib.Path(__file__).parents[0]) + "/common-args.yaml"
+
+    print("launching with arguments from " + param_file_path)
+
     return LaunchDescription([
         Node(
             package="wheel_control",
             namespace="diff_drive",
             executable="wheel_driver",
+            parameters=[
+                param_file_path
+            ]
         ),
         Node(
             package="localization",
             namespace="diff_drive",
-            executable="odometry"
+            executable="odometry",
+            parameters=[
+                param_file_path
+            ]
         )
     ])
--- robot_ws/launch/simulation.launch.py
+++ robot_ws/launch/simulation.launch.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 from os import environ
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -7,6 +8,10 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+
+    param_file_path = str(pathlib.Path(__file__).parents[0]) + "/common-args.yaml"
+
+    print("launching with arguments from " + param_file_path)
 
     env={"IGN_GAZEBO_SYSTEM_PLUGIN_PATH": environ["LD_LIBRARY_PATH"]}
 
@@ -24,12 +29,19 @@ def generate_launch_description():
             package="wheel_control",
             namespace="diff_drive",
             executable="wheel_driver",
+            parameters=[
+                param_file_path
+            ]
         ),
         Node(
             package="localization",
             namespace="diff_drive",
-            executable="odometry"
+            executable="odometry",
+            parameters=[
+                param_file_path
+            ]
         ),
+        # start the bridge to ign gazebo
         ExecuteProcess(
             cmd=[
                 "ros2 run ros_ign_bridge parameter_bridge",
